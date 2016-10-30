580aa49b1df637104ee4fc8587ea7b1d3e849bb4
Greg Wiley <aztec.rex@jammm.com>
2016-10-30 12:42:57 -0700

    parser

--- lang/src/ParseWhile.hs
+++ lang/src/ParseWhile.hs
@@ -1,10 +1,13 @@
-module ParseWhile where
+module ParseWhile (parseString) where
 
 import Control.Monad (void)
 import Text.Megaparsec
 import Text.Megaparsec.Expr
 import Text.Megaparsec.String -- input stream is of type ‘String’
 import qualified Text.Megaparsec.Lexer as L
+
+
+
 
 
 data BExpr = BoolConst Bool
@@ -70,5 +73,95 @@ identifier = (lexeme . try) (p >>= check)
                then fail $ "keyword " ++ show x ++ "cannot be an identifier"
                else return x
 
+
+whileParser :: Parser Stmt
+whileParser = sc *> stmt <* eof
+
+stmt :: Parser Stmt
+stmt = parens stmt <|> stmtSeq
+
+stmtSeq :: Parser Stmt
+stmtSeq = f <$> sepBy1 stmt' semi
+  where f l = if length l == 1 then head l else Seq l
+
+stmt' :: Parser Stmt
+stmt' = ifStmt <|> whileStmt <|> skipStmt <|> assignStmt
+
+ifStmt :: Parser Stmt
+ifStmt =
+  do rword "if"
+     cond <- bExpr
+     rword "then"
+     stmt1 <- stmt
+     rword "else"
+     stmt2 <- stmt
+     return $ If cond stmt1 stmt2
+
+whileStmt :: Parser Stmt
+whileStmt =
+  do rword "while"
+     cond <- bExpr
+     rword "do"
+     stmt1 <- stmt
+     return $ While cond stmt1
+
+assignStmt :: Parser Stmt
+assignStmt =
+  do var <- identifier
+     void $ symbol ":="
+     expr <- aExpr
+     return $ Assign var expr
+
+skipStmt :: Parser Stmt
+skipStmt = Skip <$ rword "skip"
+
+aExpr :: Parser AExpr
+aExpr = makeExprParser aTerm aOperators
+
+bExpr :: Parser BExpr
+bExpr = makeExprParser bTerm bOperators
+
+aOperators :: [[Operator Parser AExpr]]
+aOperators =
+  [ [Prefix (symbol "-" *> pure Neg) ],
+    [InfixL (symbol "*" *> pure (ABinary Multiply)),
+     InfixL (symbol "/" *> pure (ABinary Divide)) ],
+    [InfixL (symbol "+" *> pure (ABinary Add)),
+     InfixL (symbol "-" *> pure (ABinary Subtract)) ]]
+
+bOperators :: [[Operator Parser BExpr]]
+bOperators =
+  [ [Prefix (rword "not" *> pure Not) ],
+    [InfixL (rword "and" *> pure (BBinary And)),
+     InfixL (rword "or"  *> pure (BBinary Or)) ]]
+
+aTerm :: Parser AExpr
+aTerm = parens aExpr
+    <|> Var      <$> identifier
+    <|> IntConst <$> integer
+
+bTerm :: Parser BExpr
+bTerm = parens bExpr
+     <|> (rword "true" *> pure (BoolConst True))
+     <|> (rword "false" *> pure (BoolConst False))
+     <|> rExpr
+
+rExpr :: Parser BExpr
+rExpr = 
+  do a1 <- aExpr
+     op <- relation
+     a2 <- aExpr
+     return $ RBinary op a1 a2
+
+relation :: Parser RBinOp
+relation =  (symbol ">" *> pure Greater)
+        <|> (symbol "<" *> pure Less)
+
+
+parseString :: String -> Stmt
+parseString source =
+  case parse whileParser "" source of
+    Left e -> error $ show e
+    Right r -> r
 
 
