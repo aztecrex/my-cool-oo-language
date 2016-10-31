1275ac3861985b7859770f051bc68eba1d855d8c
Greg Wiley <aztec.rex@jammm.com>
2016-10-30 17:32:02 -0700

    interface

--- lang/src/Clutch/Parser.hs
+++ lang/src/Clutch/Parser.hs
@@ -1,6 +1,6 @@
 module Clutch.Parser (parseString) where
 
-import Data.Maybe (isJust, fromJust)
+import Data.Maybe (fromMaybe, isJust, fromJust)
 import Control.Monad (void)
 import Text.Megaparsec
 import Text.Megaparsec.Expr
@@ -13,6 +13,8 @@ data CompilationUnit = CompilationUnit [Statement] deriving (Show)
 data Statement =
     TypeDeclaration TypeLiteral [Statement] [Modifier]
   | Class String
+  | InterfaceDeclaration TypeLiteral (Maybe TypeLiteral) [Statement]
+  | Placeholder
   deriving (Show)
 
 data TypeLiteral = 
@@ -55,7 +57,7 @@ identifier = (lexeme . try) (p >>= check)
       check x = if elem x reservedWords
                   then fail $ "reserved word " ++ show x ++ " cannot be an identifier"
                   else return x
-      reservedWords = ["type", "class", "native"]
+      reservedWords = ["type", "class", "native", "placeholder"]
 
 
 -- Parser
@@ -71,7 +73,7 @@ statements :: Parser Statement -> Parser [Statement]
 statements p = many (p <* semi) >>= \ss -> return  ss
 
 topLevelStatement :: Parser Statement
-topLevelStatement = typeDeclaration;
+topLevelStatement = typeDeclaration <|> interfaceDeclaration;
 
 typeDeclaration :: Parser Statement
 typeDeclaration = do
@@ -93,6 +95,18 @@ typeLiteral = do
   name <- identifier
   maybeParams <- optional (angle (sepBy typeLiteral comma))
   return $ TypeLiteral name (maybe [] id maybeParams) 
+
+interfaceDeclaration :: Parser Statement
+interfaceDeclaration = do
+  reservedWord "interface"
+  ifcId <- typeLiteral
+  maybeBinding <- optional typeLiteral
+  maybeStatements <- optional (block (statements interfaceStatement))
+  return $ InterfaceDeclaration ifcId maybeBinding (fromMaybe [] maybeStatements)
+
+interfaceStatement :: Parser Statement
+interfaceStatement = reservedWord "placeholder" >>= \_ -> return Placeholder
+
 
 -- Runner
 
